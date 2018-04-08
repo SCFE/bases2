b1ef9a7a86f4bc357f4583b63ce844970be375c1
Edgar Castillo <3dg1992@gmail.com>
2018-04-08 17:48:42 -0600

    Commit con archivo para login

---
+++ php/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../public/bootstrap/dist/css/bootstrap.css">
+    <title>Pruebas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+
+   <div class='container' style="position:relative; top: 250px;  width: 400px; height: 200px;">
+   <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Direccion email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Ingresar</button>
+</form>
+    </div>
+  
+ <script src="../public/bootstrap/dist/js/bootstrap.js"></script>  
+</body>
+</html>
+
+<!--
+        include('conexion.php');
+        echo phpinfo();
+?>-->
