83357c5bce4c7d8c1a0a9dcf8935063b3a3aca7e
Edgar Castillo <3dg1992@gmail.com>
2018-04-24 04:50:46 -0600

    se agrego procedimento almacenado de carga masiva

---
+++ Querys/storedProcedure_bulkInsert.sql
@@ -0,0 +1,27 @@
+﻿
+
+--Para que utilice la base de datos prueba
+
+USE prueba
+--Creaci�n del procedimiento almacenado
+GO 
+CREATE PROCEDURE carga_masiva
+--Parametros que utilizara el procedimiento almacenado
+	@ruta nvarchar(250)
+AS
+--Query que realizara el procedimiento almacenado
+	SET NOCOUNT ON;
+	BULK INSERT USUARIO
+	FROM 'C:\Users\CloudOperator\Desktop\database\informacion.csv'
+	WITH 
+	(
+		FIELDTERMINATOR= ';',
+		ROWTERMINATOR = '\n'
+	)
+GO
+
+DELETE FROM USUARIO
+SELECT * FROM USUARIO
+	
+--Prueba del procedimiento almacenado
+carga_masiva -- 'C:\Users\CloudOperator\Desktop\database\informacion.csv'
